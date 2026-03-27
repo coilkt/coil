26e38c64b8e77072bde165f151ea6c1858cc232e
BUG Valid Broken Github <nicaulas.pur23@gmail.com>
2026-03-27 17:58:08 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "===================================================="
+echo "[!] POC: Broken Link Hijacking Vulnerability [!]"
+echo "===================================================="
+echo "Executed by: babyrootkid"
+echo "Dampak: Penyerang bisa menyisipkan malware di sini."
+echo "===================================================="
+touch spotify_vulnerable.txt
